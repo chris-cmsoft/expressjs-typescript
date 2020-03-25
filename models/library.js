5582aef0aa36a45a2a6617e0716cc108fe14f109
Christiaan Vermeulen <chrism.vermeulen@gmail.com>
2020-03-25 23:04:41 +0100

    Add libraries model & migrations

---
+++ models/library.js
@@ -0,0 +1,10 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Library = sequelize.define('Library', {
+    name: DataTypes.STRING
+  }, {});
+  Library.associate = function(models) {
+    // associations can be defined here
+  };
+  return Library;
+};
