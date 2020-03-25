b07a02a75f44f4ae69b36a35adf72454bd96e0b1
Christiaan Vermeulen <chrism.vermeulen@gmail.com>
2020-03-25 23:58:19 +0100

    Add most crud endpoints

--- src/router.js
+++ src/router.js
@@ -20,7 +20,26 @@ app.post('/libraries', async (request, response) => {
     const library = await Library.create({
         name,
     });
+    return response.status(201).json(library);
+});
+
+app.patch('/libraries/:id', async (request, response) => {
+    await Library.update(request.body, {
+        where: {
+            id: request.params.id,
+        }
+    });
+    const library = await Library.findByPk(request.params.id);
     return response.json(library);
+});
+
+app.delete('/libraries/:id', async (request, response) => {
+    await Library.destroy({
+        where: {
+            id: request.params.id,
+        }
+    });
+    return response.status(200).json('');
 });
 
 module.exports = app;
